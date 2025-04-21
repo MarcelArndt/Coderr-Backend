ddea98d051db1d6bdab0f025e6ad3a37b719aa76
Marcel Arndt <marcel.arndt@hotmail.com>
2025-04-21 12:29:14 +0200

    some migrations issues fixed

--- install.bat
+++ install.bat
@@ -10,16 +10,6 @@ call env\Scripts\activate
 echo Installiere Requirements...
 pip install -r requirements.txt
 
-if exist db.sqlite3 del db.sqlite3
-del /Q market_app\migrations\0*.py 2>nul
-del /Q auth_app\migrations\0*.py 2>nul
-
-echo Erstelle neue Migrationen...
-python manage.py makemigrations market_app
-python manage.py makemigrations auth_app
-
-echo Führe Migrationen durch...
-python manage.py migrate
 
 echo Führe utils.py aus...
 python utils.py
