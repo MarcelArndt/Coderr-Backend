860859699f5df832752a2c06f58fd3dd2bae666b
Marcel Arndt <marcel.arndt@hotmail.com>
2025-04-21 14:52:49 +0200

    some migrations issus fixed

--- coderr/urls.py
+++ coderr/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include('market_app.api.urls')),
+    path("api/", include('coderr_market_app.api.urls')),
     path("api/", include('auth_app.api.urls')),
 ]
 
